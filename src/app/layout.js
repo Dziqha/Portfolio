724ebe2090bdc85e9a5ef02239c18d0a197ee18b
Dziqha <lulussnbp01@gmail.com>
2024-08-26 06:24:42 +0700

    updated optimize metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import LatestCommit from '../components/LatestCommit';
 import Image from 'next/image';
-import Head from 'next/head';
 
 // Importing Inter font with weights 100 to 900 and display swap
 const inter = Inter({
@@ -20,27 +19,36 @@ const jetBrainsMono = JetBrains_Mono({
   display: "swap",
 });
 
+export const metadata = {
+  title: 'Portfolio',
+  charset: 'UTF-8',
+  viewport: 'width=device-width, initial-scale=1.0',
+  description: "Haadziq's personal website.",
+  keywords: 'Haadziq, Dziqha, Portfolio, dziqha, haadziq, portfolio, personal, personal website, programmer portfolio, coding portfolio, programmer',
+  author: 'Dziqha',
+  url: 'https://haadziq.my.id',
+  'identifier-URL': 'https://haadziq.my.id',
+  twitter: {
+    title: 'haadziq.my.id',
+    description: "Haadziq's personal website.",
+    image: '/foto.png',
+    card: '/foto.png',
+  },
+  openGraph: {
+    title: 'haadziq.my.id',
+    description: "Haadziq's personal website.",
+    image: '/foto.png',
+    url: 'https://haadziq.my.id',
+    site_name: 'haadziq.my.id',
+    type: 'website',
+  },
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>haadziq.my.id</title>
-        <meta name="description" content="Haadziq's personal website." />
-        <meta name="keywords" content="Haadziq, Dziqha, Portfolio, dziqha, haadziq, portfolio, personal, personal website, progammer portfolio, coding portfolio, programmer" />
-        <meta name="author" content="Dziqha" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="haadziq.my.id" />
-        <meta property="og:description" content="Haadziq's personal website." />
-        <meta property="og:image" content="https://haadziq.my.id/foto.png" />
-        <meta property="og:url" content="https://haadziq.my.id" />
-        <meta property="og:site_name" content="haadziq.my.id" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:title" content="haadziq.my.id" />
-        <meta name="twitter:description" content="Haadziq's personal website." />
-        <meta name="twitter:image" content="https://haadziq.my.id/foto.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <link rel="icon" href="/about me.png" />
-      </Head>
+      <head><link rel="icon" href="/about me.png" /></head>
       <body suppressHydrationWarning={true} className={`${inter.className} ${jetBrainsMono.className} bg-gruvbox-dark text-gruvbox-milk`}>
         <div className="sm:pt-20 max-w-screen-lg mx-auto p-5 relative">
           <div className="z-0 absolute -mt-5 right-5 opacity-30 select-none">
